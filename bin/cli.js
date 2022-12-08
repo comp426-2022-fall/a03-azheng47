00461cf12a90fce1b2309cd54ae2abca4cdfce41
Andrew Zheng <azswim@live.unc.edu>
2022-12-08 01:41:42 -0500

    simulate random dice roll

--- bin/cli.js
+++ bin/cli.js
@@ -5,7 +5,7 @@ function roll(sides, dice, rolls) {
 	for (let i = 0; i < rolls; i++) {
 		let total = 0;
 		for (let j = 0; j < dice; j++) {
-
+			let val = Math.floor(Math.random() * this.sides) + 1;
 		}
 	}
 }
