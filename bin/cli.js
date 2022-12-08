38df29ef9e475b3913b7a72e0cd2275e68850078
Andrew Zheng <azswim@live.unc.edu>
2022-12-08 02:19:32 -0500

    comment for providing default values

--- bin/cli.js
+++ bin/cli.js
@@ -7,6 +7,7 @@ import minimist from "minimist";
 // grab command line arguments
 let args = minimist(process.argv.slice(2));
 
+// extract the cli arg values or give them a default value if not present
 let sides = args.sides || 6;
 let dice = args.dice || 2;
 let rolls = args.rolls || 1;
